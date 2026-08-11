ec83d6c9656c7aa3b4a4b611847af751ab927574
Hrutvik Pamuluri <hrutvikvrp@gmail.com>
2026-08-11 16:25:52 -0400

    Add semantic HTML structure with Header, Nav, Main, and Footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Header from './Header.js';
+import Nav from './Nav.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+}
 
-  function App() {
-    return (
-      <div className="App">
-        Homepage
-      </div>
-    );
-  }
-
-  export default App;
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import logo from './Logo.svg';
+function Footer() {
+  return (
+    <footer>
+        <img src={logo} alt="logo" />
+        <nav>
+            <h3>Doormat Navigation</h3>
+            <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#reservations">Reservations</a></li>
+            <li><a href="#orderonline">Order Online</a></li>
+            <li><a href="#login">Login</a></li>
+        </ul>
+        </nav>
+        <address>
+            <h3>Contact</h3>
+            <p>Address</p>
+            <p>Phone</p>
+            <p>Email</p>
+        </address>
+    </footer>
+  );
+}
+
+export default Footer;
