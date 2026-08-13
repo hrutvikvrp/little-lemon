e86a99eb77cd04e7c139be25e6a12a372bf764e8
Hrutvik Pamuluri <hrutvikvrp@gmail.com>
2026-08-13 11:33:39 -0400

    Add HTML5 and React client-side form validation

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -9,22 +9,25 @@ function BookingForm(props) {
         props.submitForm({ date, time, guests, occasion });
         console.log({ date, time, guests, occasion });
     }
+    function valid() {
+        return ((date !== "") && (time !== "") && ((Number(guests) > 0) && (Number(guests) < 11 ) && (occasion !== "")));
+    }
     return (
         <form onSubmit={submitHandler} style={{ display: "grid", maxWidth: "200px", gap: "20px" }}>
             <label htmlFor="res-date">Choose date</label>
-            <input type="date" id="res-date" value={date} onChange={e => { setDate(e.target.value); props.dispatch(e.target.value) }} />
+            <input type="date" id="res-date" value={date} required min = {new Date().toISOString().split("T")[0]} onChange={e => { setDate(e.target.value); props.dispatch(e.target.value) }} />
             <label htmlFor="res-time">Choose time</label>
-            <select id="res-time" value={time} onChange={e => setTime(e.target.value)}>
+            <select id="res-time" value={time} required onChange={e => setTime(e.target.value)}>
                 {props.availableTimes.map(t => <option key={t}>{t}</option>)}
             </select>
             <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)} />
+            <input type="number" required placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)} />
             <label htmlFor="occasion">Occasion</label>
-            <select id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)}>
+            <select id="occasion" required value={occasion} onChange={e => setOccasion(e.target.value)}>
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
-            <input type="submit" value="Make Your reservation" />
+            <input type="submit" value="Make Your reservation" disabled={!valid()}/>
         </form>
     );
 }
--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,11 +1,11 @@
 import BookingForm from './BookingForm'
 function BookingPage(props) {
-  return (
-    <>
-    <h1>Book a Table</h1>
-    <BookingForm availableTimes={props.availableTimes} dispatch={props.dispatch} submitForm={props.submitForm}/>
-    </>
-  );
+    return (
+        <>
+            <h1>Book a Table</h1>
+            <BookingForm availableTimes={props.availableTimes} dispatch={props.dispatch} submitForm={props.submitForm} />
+        </>
+    );
 }
 
 export default BookingPage;
